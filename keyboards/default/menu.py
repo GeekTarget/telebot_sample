be4cf23727ae13934d7fa4dfddc6646eb7975ae5
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2021-08-25 20:59:35 +0400

    Create menu.py

---
+++ keyboards/default/menu.py
@@ -0,0 +1,9 @@
+# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# testButton = ReplyKeyboardMarkup(
+#     keyboard=[
+#         [KeyboardButton(text='Первая кнопка'),
+#          KeyboardButton(text='Вторая кнопка')]
+#     ],
+#     resize_keyboard=True
+# )
