a4f2f0393dcd36f5c5315e3bf3a7c0a388919298
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2021-09-07 10:26:31 +0400

    Create mini_buttons.py

---
+++ keyboards/inline/mini_buttons.py
@@ -0,0 +1,7 @@
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# cancel_btn = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [InlineKeyboardButton(text='Отмена', callback_data='cancel')]
+#     ]
+# )
